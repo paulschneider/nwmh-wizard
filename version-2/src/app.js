e3a5ed78bd97db07b22922c916e219b77d99aa38
Paul Schneider <paul.schneider@iconinc.com.au>
2017-03-07 10:02:25 +1100

    - flattening repo to remove legacy version

--- version-2/src/app.js
+++
@@ -1,7 +0,0 @@
-import App from "./classes/App";
-
-window.NWMH_Wizard = function() {
-	let Wizard = new App;
-
-	Wizard.init();
-}
--- version-2/src/config/Externals.js
+++
@@ -1,8 +0,0 @@
-export const Externals = Object.freeze({
-	"young": "/home",
-	"adult": "/home",
-	"senior": "/home",
-	"what-to-expect": "/home",
-	"speciality-services": "/home",
-	"support-for-families": "/home",
-});
